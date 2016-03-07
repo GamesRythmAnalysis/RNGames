e37a6a43607cfc82449e65dc36908e4a700c57a3
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2016-03-07 13:53:51 +0100

    Initial commit.

---
+++ src/main/java/fr/utbm/RNGames/App.java
@@ -0,0 +1,13 @@
+package fr.utbm.RNGames;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
