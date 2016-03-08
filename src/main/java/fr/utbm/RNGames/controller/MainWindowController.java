1bd7140a268b80aa31299939486bb0fca666aa67
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2016-03-08 13:03:36 +0100

    Linked writers and GUI.

--- src/main/java/fr/utbm/RNGames/controller/MainWindowController.java
+++ src/main/java/fr/utbm/RNGames/controller/MainWindowController.java
@@ -1,8 +1,12 @@
 package fr.utbm.RNGames.controller;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 import fr.utbm.RNGames.App;
+import fr.utbm.RNGames.keyboard.KeyboardWriter;
+import fr.utbm.RNGames.mouse.MouseWriter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,6 +15,8 @@ import javafx.scene.control.ToggleButton;
 import javafx.stage.DirectoryChooser;
 
 public class MainWindowController {
+	private KeyboardWriter kWriter;
+	private MouseWriter mWriter;
 
 	@FXML
 	private TextField textAreaSaveDirectory;
@@ -54,7 +60,7 @@ public class MainWindowController {
 	@FXML
 	private void handleSelectFolder() {
 		final DirectoryChooser chooser = new DirectoryChooser();
-		chooser.setTitle("Select directory where records are saved");
+		chooser.setTitle("Select directory where records are saved"); //$NON-NLS-1$
 
 		if (!this.textAreaSaveDirectory.getText().equals("")) { //$NON-NLS-1$
 			final File defaultDirectory = new File(this.textAreaSaveDirectory.getText());
@@ -78,6 +84,26 @@ public class MainWindowController {
 			return;
 		}
 
+		if (this.toggleButtonKeyboard.isSelected()) {
+			try {
+				this.kWriter = new KeyboardWriter(new URL("file://" + this.textAreaSaveDirectory.getText() + "/keyboard.csv")); //$NON-NLS-1$ //$NON-NLS-2$
+				this.kWriter.start();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		if (this.toggleButtonMouse.isSelected()) {
+			try {
+				this.mWriter = new MouseWriter(new URL("file://" + this.textAreaSaveDirectory.getText() + "/mouse.csv")); //$NON-NLS-1$ //$NON-NLS-2$
+				this.mWriter.start();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 		this.textAreaSaveDirectory.disableProperty().set(true);
 		this.textAreaRecordName.disableProperty().set(true);
 		this.buttonSelectDirectory.disableProperty().set(true);
@@ -98,6 +124,14 @@ public class MainWindowController {
 		this.toggleButtonMouse.disableProperty().set(false);
 		this.buttonStartRecording.disableProperty().set(false);
 		this.buttonStopRecording.disableProperty().set(true);
+
+		if (this.kWriter != null) {
+			this.kWriter.stop();
+		}
+
+		if (this.mWriter != null) {
+			this.mWriter.stop();
+		}
 	}
 
 }
