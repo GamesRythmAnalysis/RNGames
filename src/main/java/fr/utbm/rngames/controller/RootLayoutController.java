a9fea88661132a681a0be59689ca2c0866b5a4ea
Benoît CORTIER <benoit.cortier@gmail.com>
2016-03-16 16:41:55 +0100

    Removed unused import.

--- src/main/java/fr/utbm/rngames/controller/RootLayoutController.java
+++ src/main/java/fr/utbm/rngames/controller/RootLayoutController.java
@@ -1,7 +1,6 @@
 package fr.utbm.rngames.controller;
 
 import fr.utbm.rngames.event.EventDispatcher;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
