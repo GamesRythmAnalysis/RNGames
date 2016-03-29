aecf8d34eebd091e7b3db2a9e2aee2e3d0d8faea
Benoît CORTIER <benoit.cortier@gmail.com>
2016-03-29 12:18:27 +0200

    Added license.

--- src/main/java/fr/utbm/rngames/event/Event.java
+++ src/main/java/fr/utbm/rngames/event/Event.java
@@ -1,3 +1,8 @@
+/**************************************************************************
+ * EventDispatcher, to link event senders and event listeners.
+ * Copyright (C) 2016  BOULMIER Jérôme
+ *************************************************************************/
+
 package fr.utbm.rngames.event;
 
 public interface Event<T> {
