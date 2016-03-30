566fe8033ee616bb5a8e24d4f69003b903d3af33
Benoît CORTIER <benoit.cortier@gmail.com>
2016-03-30 16:14:32 +0200

    Removed default file template in EventListener class.

--- src/main/java/fr/utbm/rngames/controller/CloseEventListener.java
+++ src/main/java/fr/utbm/rngames/controller/CloseEventListener.java
@@ -20,9 +20,6 @@ package fr.utbm.rngames.controller;
 
 import java.util.EventListener;
 
-/**
- * Created by lomadriel on 3/16/16.
- */
 public interface CloseEventListener extends EventListener {
 	void handleCloseEvent();
 }
